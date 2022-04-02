53fbf9e590ba4d0ffc5580cb2e9c9f55c0710ac9
nguyenphuhongloan <nguyenphuhongloan@gmail.com>
2022-04-03 03:10:40 +0700

    [Add]: asdjhasdkjhsa

--- assets/js/command.js
+++ assets/js/command.js
@@ -20,11 +20,11 @@ function printCommand(pageNumber) {
         '        </tr>' +
         '    </thead>' +
         '    <tbody> ';
-    let start=5*(pageNumber-1);
-    for (let i = start; i < arr.length && i< start+5 ; i++) {
+    let start = 5 * (pageNumber - 1);
+    for (let i = start; i < arr.length && i < start + 5; i++) {
         s +=
             '<tr  >' +
-            '    <td scope="row" class="text-center"  >' + Number(i+1) + '</td>' +
+            '    <td scope="row" class="text-center"  >' + Number(i + 1) + '</td>' +
             '    <td class="text-center" >' + arr[i].name + '</td>' +
             '    <td><div  class="rounded " style= "background-color:#45CCAF"> <p class="text-center" style="color: black" >' + arr[i].hotkey + '</p></div></td>' +
             '</tr>';
@@ -33,14 +33,94 @@ function printCommand(pageNumber) {
         '</tbody >' +
         '</table >';
     document.getElementById('content').innerHTML = s;
-    let tmp = Math.ceil(arr.length/5);
-    
-    let s2=""; 
-    for(let i=1;i<=tmp;i++){
-        s2 += '<input type="button" class=" m-2" value="' + i +'" onclick="printCommand('+i+')">';
+    let tmp = Math.ceil(arr.length / 5);
+
+    let s2 = "";
+    for (let i = 1; i <= tmp; i++) {
+        s2 += '<input type="button" class=" m-2" value="' + i + '" onclick="printCommand(' + i + ')">';
     }
     document.getElementById('PageNum').innerHTML = s2;
 };
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }
+});
+let hotKey;
+let tmp = "";
+$("#btn-clipboard").click(function () {
+    if ($("#btn-clipboard").val() == "Bắt đầu") {
+        $("#text-command").removeClass("d-none");
+        $("#btn-clipboard").val("Kết thúc");
+        $("#btn-clipboard-reset").hide();
+        let str = "";
+        hotkeys('*', function (event, handler) {
+            // console.log(event);
+            if (event.ctrlKey == true) {
+                str += '"Ctrl" ';
+            }
+            if (event.shiftKey == true) {
+                str += '"Shift" ';
+            }
+            if (event.ctrlKey == true || event.shiftKey == true) {
+                if (event.key != "Control" && event.key != "Shift") {
+                    hotKey = event;
+                    tmp = event.key;
+                    str += '"' + event.key + '"';
+                }
+                else {
+                    str = "";
+                    hotKey = null;
+                    tmp = "";
+                }
+            }
+            else {
+                str = "";
+                hotKey = null;
+                tmp = "";
+            }
+            $("#text-command").val(str);
+
+        });
+    } else {
+        //$("#text-command").val(str);
+        $("#btn-clipboard").val("Bắt đầu");
+        $("#btn-clipboard-reset").show();
+
+        //console.log(str)
+    }
+});
+$("#btn-clipboard-reset").click(function () {
+    $("#text-command").val(str);
+
+});
+$("#add").click(function () {
+    if (tmp == "") {
+        return;
+    }
+    var arr = [];
+    if (hotKey.ctrlKey == true) {
+        arr.push("ctrl");
+    }
+    if (hotKey.shiftKey == true) {
+        arr.push("shift");
+    }
+    arr.push(hotKey.key.toLowerCase())
+    let command = {
+        "name": $("#name-command").val(),
+        "hotKey": arr
+    }
+    console.log(command);
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i].hotKey==command.hotKey || arr[i].name==command.name) {
+            alert("Hotkey đã tồn tại");
+            return;
+        }
+    }
+    arr.push(command);
+
+})
+
 window.onload = function () {
 
     printCommand(1);
---
+++ assets/js/setting.js
@@ -0,0 +1,55 @@
+const arr =
+    [
+        { "action": { "id": 1, "name": "action1" }, "command": { "id": 1, "name": "command1" } },
+        {"action":{"id":2,"name": "action2"}, "command":{"id":2,"name": "command10"} },
+        {"action":{"id":3,"name": "action3"}, "command":{"id":3,"name": "command2"} },
+        {"action":{"id":4,"name": "action4"}, "command":{"id":4,"name": "command3"} },
+        {"action":{"id":5,"name": "action5"}, "command":{"id":5,"name": "command4"} },
+        {"action":{"id":6,"name": "action6"}, "command":{"id":6,"name": "command5"} },
+        {"action":{"id":7,"name": "action7"}, "command":{"id":7,"name": "command6"} },
+        {"action":{"id":8,"name": "action8"}, "command":{"id":8,"name": "command7"} },
+        {"action":{"id":9,"name": "action9"}, "command":{"id":9,"name": "command8"} },
+        {"action":{"id":10,"name": "action10"}, "command":{"id":10,"name": "command9"} },
+        {"action":{"id":11,"name": "action11"}, "command":{"id":11,"name": "command11"} },
+        { "action": { "id": 12, "name": "action12" }, "command": { "id": 12, "name": "command12" } },
+    ];
+function printSetting(pageNumber) {
+    let s =
+        '<table class="table table-dark table-striped ">' +
+        '    <thead class="thead-dark">' +
+        '        <tr>' +
+        '            <th scope="col"><p class="text-center"> ID </p></th>' +
+        '            <th scope="col"><p class="text-center"> Action </p></th>' +
+        '            <th scope="col"><p class="text-center"> Command </p> </th>' +
+        '            <th ></th>' +
+        
+        '        </tr>' +
+        '    </thead>' +
+        '    <tbody> ';
+    let start = 5 * (pageNumber - 1);
+    for (let i = start; i < arr.length && i < start + 5; i++) {
+        s +=
+            '<tr  >' +
+            '    <td scope="row" class="text-center"  >' + Number(i + 1) + '</td>' +
+        '    <td class="text-center" >' + arr[i].action.name + '</td>' +
+        '    <td class="text-center" >' + arr[i].command.name + '</td>' +
+        
+            '</tr>';
+    }
+    s +=
+        '</tbody >' +
+        '</table >';
+    document.getElementById('content').innerHTML = s;
+    let tmp = Math.ceil(arr.length / 5);
+
+    let s2 = "";
+    for (let i = 1; i <= tmp; i++) {
+        s2 += '<input type="button" class=" m-2" value="' + i + '" onclick="printSetting(' + i + ')">';
+    }
+    document.getElementById('PageNum').innerHTML = s2;
+};
+
+window.onload = function () {
+
+    printSetting(1);
+};
