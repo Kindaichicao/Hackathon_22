009b478cb0ef3ca3041b9aa37dca4df76b69b86c
nguyenphuhongloan <nguyenphuhongloan@gmail.com>
2022-04-02 23:24:31 +0700

    Paging

--- assets/js/command.js
+++ assets/js/command.js
@@ -9,7 +9,7 @@ const arr =
         { "name": "Save", "hotkey": "Ctrl+s" },
         { "name": "Switch Window", "hotkey": "Alt + Tab" }
     ];
-function printCommand() {
+function printCommand(pageNumber) {
     let s =
         '<table class="table table-dark table-striped ">' +
         '    <thead class="thead-dark">' +
@@ -20,10 +20,11 @@ function printCommand() {
         '        </tr>' +
         '    </thead>' +
         '    <tbody> ';
-    for (let i = 0; i < arr.length; i++) {
+    let start=5*(pageNumber-1);
+    for (let i = start; i < arr.length && i< start+5 ; i++) {
         s +=
             '<tr  >' +
-            '    <td scope="row">' + i + '</td>' +
+            '    <td scope="row">' + Number(i+1) + '</td>' +
             '    <td>' + arr[i].name + '</td>' +
             '    <td><div  class="rounded " style= "background-color:#45CCAF"> <p class="text-center" style="color: black" >' + arr[i].hotkey + '</p></div></td>' +
             '</tr>';
@@ -32,8 +33,15 @@ function printCommand() {
         '</tbody >' +
         '</table >';
     document.getElementById('content').innerHTML = s;
+    let tmp = Math.ceil(arr.length/5);
+    
+    let s2=""; 
+    for(let i=1;i<=tmp;i++){
+        s2 += '<input type="button" class=" m-2" value="' + i +'" onclick="printCommand('+i+')">';
+    }
+    document.getElementById('PageNum').innerHTML = s2;
 };
 window.onload = function () {
 
-    printCommand();
+    printCommand(1);
 };
